ba49c8794ca7faacec8952868a06503002c729fa
Johnny Silva <johnnysilva.dev@gmail.com>
2024-05-23 11:03:41 -0300

    feat: Angular NgRx Signal Store Crash Course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosStore } from './store/todos.store';
+import { JsonPipe } from '@angular/common';
+import { TodosListComponent } from './todos-list/todos-list.component';
 
+import { MatSpinner } from "@angular/material/progress-spinner";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    JsonPipe,
+    TodosListComponent,
+    MatSpinner
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ngrx-signal-store-crash-course';
+export class AppComponent implements OnInit {
+  store = inject(TodosStore);
+
+
+  ngOnInit() {
+    this.loadTodos()
+      .then(() => {
+        console.log("Todos loaded");
+      })
+  }
+
+  async loadTodos() {
+    await this.store.loadAll();
+  }
+
 }
---
+++ src/app/model/mock-data.ts
@@ -0,0 +1,44 @@
+import { Todo } from "./todo.model";
+
+export const TODOS: Todo[] = [
+  {
+    id: '1',
+    title: 'Learn Angular',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'Learn Ngrx',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'Learn Ngrx Signal Store',
+    completed: false
+  },
+  {
+    id: '4',
+    title: 'Learn Ngrx Effects',
+    completed: false
+  },
+  {
+    id: '5',
+    title: 'Learn Ngrx Entity',
+    completed: false
+  },
+  {
+    id: '6',
+    title: 'Learn Ngrx Store',
+    completed: false
+  },
+  {
+    id: '7',
+    title: 'Learn Ngrx Router',
+    completed: false
+  },
+  {
+    id: '8',
+    title: 'Learn Ngrx Form',
+    completed: false
+  },
+]
